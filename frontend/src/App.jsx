88c354d7af173b347cfd919393a3d5812615704f
Pankaj Panday <ppanday064@gmail.com>
2024-03-01 20:29:31 +0530

    day 6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,12 @@ const router = createBrowserRouter(
 					<h1 className="mt-20 text-center text-3xl">
 						Front end not ready yet
 					</h1>
+					<a
+						href="https://github.com/Pankaj-Panday/food-recipe-app"
+						className="text-center underline block mt-6 text-blue-600"
+					>
+						Github link to backend code
+					</a>
 				</div>
 			}
 		></Route>
