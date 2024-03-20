ba0f55979348135244b3b61f3618707f63ca7725
Pankaj Panday <ppanday064@gmail.com>
2024-03-20 17:51:29 +0530

    day 22

--- backend/src/controllers/review.controller.js
+++ backend/src/controllers/review.controller.js
@@ -4,27 +4,27 @@ import ApiResponse from "../utils/ApiResponse.js";
 import { Review } from "../models/review.model.js";
 import { Recipe } from "../models/recipe.model.js";
 
+function validateReviewData(rating, comment) {
+  if (!rating || !parseInt(rating)) {
+    throw new ApiError(400, "Missing or invalid argument: rating");
+  }
+  if (rating < 1 || rating > 5) {
+    throw new ApiError(400, "Rating should be between 1 and 5");
+  }
+  if(comment) {
+    if(comment.length > 250) {
+      throw new ApiError(400, "Comment should be under 250 characters");
+    }
+  }
+  return { rating: parseInt(rating), comment: comment || "" };
+}
+
 const createReview = asyncHandler(async (req, res) => {
 	const userId = req.user._id;
 	const recipeId = req.params.recipeId;
-	const rating = req.body?.rating;
-	const comment = req.body?.comment;
-	if (!recipeId) {
-		throw new ApiError(400, "Missing required parameter: recipeId");
-	}
-	if (!rating || rating.trim() === "" || !parseInt(rating)) {
-		throw new ApiError(400, "Missing or invalid argument: rating");
-	}
-	if (rating < 1 || rating > 5) {
-		throw new ApiError(400, "Rating should be between 1 and 5");
-	}
-	if (comment) {
-		if (comment.length > 250) {
-			throw new ApiError(400, "Comment should be under 250 characters");
-		}
-	}
+	const {rating, comment} = validateReviewData(req.body?.rating, req.body?.comment);
 
-	const recipe = await Recipe.findById(recipeId);
+  const recipe = await Recipe.findById(recipeId);
 	if (!recipe) {
 		throw new ApiError(404, "Recice not found");
 	}
@@ -78,26 +78,8 @@ const getSingleReview = asyncHandler(async (req, res) => {
 const updateReview = asyncHandler(async (req, res) => {
 	const userId = req.user._id;
 	const reviewId = req.params._id;
-	const rating = req.body?.rating;
-	const comment = req.body?.comment;
-	if (!reviewId) {
-		throw new ApiError(400, "Missing required parameter: reviewId");
-	}
-	if (!rating || rating.trim() === "" || !parsetInt(rating)) {
-		throw new ApiError(400, "Missing or invalid argument: rating");
-	}
-  if (rating < 1 || rating > 5) {
-		throw new ApiError(400, "Rating should be between 1 and 5");
-	}
-  if(comment) {
-    if(comment.length > 250) {
-      throw new ApiError(400, "Comment should be under 250 characters");
-    }
-  }
-	const review = await Review.findById(reviewId).populate({
-    path: "owner",
-    select: {name: 1}
-  });
+	const {rating, comment} = validateReviewData(req.body?.rating, req.body?.comment);
+	const review = await Review.findById(reviewId);
 	if (!review) {
 		throw new ApiError(404, "Review not found");
 	}
@@ -109,6 +91,42 @@ const updateReview = asyncHandler(async (req, res) => {
   const updatedReview = await review.save();
   return res.status(200).json(new ApiResponse(200, updatedReview, "Review updated successfully"));
 });
+
+const getAllReviewsOfRecipe = asyncHandler(async(req, res) => {
+  const recipeId = req.params.recipeId;
+  if(!recipeId) {
+    throw new ApiError(400, "Missing required parameter: recipeId");
+  }
+  const recipeObjectId = new mongoose.Types.ObjectId(recipeId);
+  const pipeline = [ 
+    {
+      $match: {recipe: recipeObjectId}
+    },
+    {
+      
+    }
+  ];
+  const reviewAggregate = Review.aggregate(pipeline);
+  const options = {
+    offset: 0,
+    limit: 5,
+  }
+  const result = await Review.aggregatePaginate(reviewAggregate, options);
+  const {totalDocs, docs, limit, offset, hasPrevPage, hasNextPage, prevPage, nextPage} = result;
+
+  const response = {
+    reviews: docs,
+    totalReviewsCount: totalDocs,
+    reviewsShown: limit,
+    offset,
+    hasPrevPage,
+    hasNextPage,
+    prevPage,
+    nextPage
+  }
+  
+  return res.status(200).json(new ApiResponse(200, response, "reviews fetched successfully"));
+})
 
 export { createReview, deleteReview, getSingleReview, updateReview };
 
