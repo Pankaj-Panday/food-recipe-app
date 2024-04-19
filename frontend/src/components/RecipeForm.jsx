ce4d3d3572500091ad616dccb29b97749ecc4956
Pankaj Panday <ppanday064@gmail.com>
2024-04-19 20:13:27 +0530

    day 39

--- frontend/src/components/RecipeForm.jsx
+++ frontend/src/components/RecipeForm.jsx
@@ -1,20 +1,169 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { Button, Container, Input, Textarea } from "./index.js";
 
 const RecipeForm = () => {
+	const {
+		register,
+		handleSubmit,
+		control,
+		formState: { errors: frontEndError },
+	} = useForm({
+		defaultValues: {
+			title: "",
+			introduction: "",
+			cookingTime: "",
+			ingredients: [{ ingredient: "" }], // here
+			steps: [{ step: "" }], // and here
+		},
+	});
+
+	const {
+		fields: ingredientFields,
+		append: appendIngredient,
+		remove: removeIngredient,
+	} = useFieldArray({
+		name: "ingredients",
+		control,
+	});
+
+	const {
+		fields: stepFields,
+		append: appendStep,
+		remove: removeStep,
+	} = useFieldArray({
+		name: "steps",
+		control,
+	});
+
+	// console.log("Ingredients array", ingredientFields);
+	// console.log(stepFields);
+	// console.log(frontEndError);
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
+	const onError = (error, e) => {
+		console.log(error);
+		if (error.ingredients) {
+			const ingredientInput = error.ingredients.find((ingredient) => {
+				if (ingredient) {
+					return ingredient.ref;
+				}
+				return false;
+			});
+
+			ingredientInput.focus();
+		}
+	};
+
 	return (
 		<article className="py-6 w-[min(400px,95%)] min-w-[300px] sm:w-4/5 max-w-[660px] mx-auto">
 			<div className="bg-gray-50 rounded-lg p-5">
 				<h2 className="text-3xl font-bold mb-4">Add a recipe</h2>
-				<form className="flex flex-col gap-4">
-					<Input label="Recipe Title" className="" required />
-					<Textarea label="Description" />
-					<Input label="Photo" type="file" />
-
-					{/* ingredients - dynamic field */}
-					{/* steps - dynamic fields */}
-					<Input label="Cooking Time" required />
+				<form
+					onSubmit={handleSubmit(onSubmit, onError)}
+					className="flex flex-col gap-4"
+					noValidate
+				>
+					<Input
+						label="Recipe Title"
+						placeholder="e.g. Paneer tikka masala"
+						className=""
+						required
+						{...register("title", {
+							required: "Recipe title is required",
+							pattern: {
+								value: /^[a-zA-Z0-9\s/-]+$/,
+								message:
+									"Only (a-z, A-Z, 0-9,-, /) characters are allowed in recipeTitle",
+							},
+						})}
+					/>
+					<Textarea
+						label="Description"
+						placeholder="Share something special about your recipe"
+						{...register("introduction")}
+					/>
+					<Input
+						label="Photo"
+						type="file"
+						accept="image/png, image/jpeg"
+						{...register("recipePhoto")}
+					/>
+					<ul className="flex flex-col gap-2">
+						{ingredientFields.map((field, index) => {
+							const label = index === 0 ? "Ingredients" : null;
+							return (
+								<li key={field.id} className="flex gap-2 items-center">
+									<Input
+										label={label}
+										placeholder="e.g. 250g paneer"
+										required
+										{...register(`ingredients.${index}.ingredient`, {
+											required: "Ingredient cannot be empty",
+										})}
+									/>
+									{index > 0 && (
+										<Button
+											type="button"
+											onClick={() => removeIngredient(index)}
+										>
+											Remove
+										</Button>
+									)}
+								</li>
+							);
+						})}
+					</ul>
+					<Button
+						className="w-12 py-1.5 px-2 rounded-lg"
+						onClick={() => appendIngredient({ ingredient: "" })}
+					>
+						Add
+					</Button>
+					<ul className="flex flex-col gap-2">
+						{stepFields.map((field, index) => {
+							const label = index === 0 ? "Steps" : null;
+							return (
+								<li key={field.id} className="flex gap-2 items-center">
+									<Input
+										label={label}
+										required
+										{...register(`steps.${index}.step`, {
+											required: "Step cannot be empty",
+										})}
+									/>
+									{index > 0 && (
+										<Button onClick={() => removeStep(index)}>Remove</Button>
+									)}
+								</li>
+							);
+						})}
+					</ul>
+					<Button
+						className="w-12 py-1.5 px-2 rounded-lg"
+						onClick={() => appendStep({ step: "" })}
+					>
+						Add
+					</Button>
+					<Input
+						type="number"
+						label="Cooking Time (in mins)"
+						placeholder="e.g. 25 (for 25 minutes)"
+						required
+						{...register("cookingTime", {
+							required: "Cooking time is required",
+							valueAsNumber: true, // it runs before validation and returns NaN if value != number
+							min: { value: 0, message: "Cooking time cannot be less than 0" },
+							validate: {
+								checkNumber: (value) =>
+									!isNaN(value) ||
+									"Enter valid cooking time in minutes (e.g. 25)",
+							},
+						})}
+					/>
 					<div className="flex gap-3 mt-2">
 						<Button
 							type="submit"
--- frontend/src/components/Textarea.jsx
+++ frontend/src/components/Textarea.jsx
@@ -1,27 +1,30 @@
-import React, { useId } from "react";
+import React, { useId, forwardRef } from "react";
 
-const Textarea = ({ label, newLine = true, required, className, ...props }) => {
-	const id = useId();
+const Textarea = forwardRef(
+	({ label, newLine = true, required, className, ...props }, ref) => {
+		const id = useId();
 
-	const textareaClass = newLine ? "w-full block mt-1.5" : null;
-	let requiredClass = required
-		? "after:content-['*'] after:text-red-500 after:ml-0.5"
-		: "";
+		const textareaClass = newLine ? "w-full block mt-1.5" : null;
+		let requiredClass = required
+			? "after:content-['*'] after:text-red-500 after:ml-0.5"
+			: "";
 
-	return (
-		<div className="w-full">
-			{label && (
-				<label htmlFor={id} className={`${requiredClass}`}>
-					{label}
-				</label>
-			)}
-			<textarea
-				id={id}
-				className={`border-2 px-2 py-1 outline-none focus:outline-none focus:border-brand-primary duration-200 focus:bg-slate-50 w-full ${textareaClass} ${className}`}
-				{...props}
-			></textarea>
-		</div>
-	);
-};
+		return (
+			<div className="w-full">
+				{label && (
+					<label htmlFor={id} className={`${requiredClass}`}>
+						{label}
+					</label>
+				)}
+				<textarea
+					id={id}
+					className={`border-2 px-2 py-1 outline-none focus:outline-none focus:border-brand-primary duration-200 focus:bg-slate-50 w-full ${textareaClass} ${className}`}
+					ref={ref}
+					{...props}
+				></textarea>
+			</div>
+		);
+	}
+);
 
 export default Textarea;
