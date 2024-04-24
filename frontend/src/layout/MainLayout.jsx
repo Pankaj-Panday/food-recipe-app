2b1538fc572d63e564a5e197c3d16f6489abe240
Pankaj Panday <ppanday064@gmail.com>
2024-04-24 19:36:39 +0530

    day 42 - part 2

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -16,7 +16,27 @@ const MainLayout = () => {
 				dispatch(userLogin(data));
 			})
 			.catch((error) => {
-				dispatch(userLogout());
+				if (error?.reason === "jwt expired" && error?.statusCode === 401) {
+					// Access token expired, try refreshing
+					userService
+						.refreshAccessTokenOfUser()
+						.then(() => {
+							userService
+								.currentUser()
+								.then(({ data }) => dispatch(userLogin(data)))
+								.catch((err) => {
+									// fetching user data after refresh
+									console.error(
+										"Failed to get current user after refreshing access token"
+									);
+									dispatch(userLogout());
+								});
+						})
+						.catch((err) => {
+							console.error("Failed to refresh access token");
+							dispatch(userLogout());
+						});
+				}
 			})
 			.finally(() => {
 				setLoading(false);
@@ -26,7 +46,7 @@ const MainLayout = () => {
 	return loading ? (
 		<AppLoading />
 	) : (
-		<div className="flex flex-col justify-between min-h-screen relative overflow-hidden">
+		<div className="relative flex flex-col justify-between min-h-screen overflow-hidden">
 			<Header />
 			<main className="pt-20">
 				<Outlet />
@@ -38,8 +58,8 @@ const MainLayout = () => {
 
 const AppLoading = () => {
 	return (
-		<div className="min-h-scree flex justify-center">
-			<h3 className="text-gray-500 text-4xl text-center mt-10">...Loading</h3>
+		<div className="flex justify-center min-h-scree">
+			<h3 className="mt-10 text-4xl text-center text-gray-500">...Loading</h3>
 		</div>
 	);
 };
